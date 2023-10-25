9f1d30ecda92e9c056943f0d8c95c1c2b58832af
anindyaprayoga <anindyaprayoga127@gmail.com>
2023-10-25 08:05:34 +0700

    Menambahkan program penjumlahan

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Hasil penjumlahan dari " + a + " dan " + b + " adalah " + sum);
+    }
+}
